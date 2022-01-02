7e635bdccd92dc82a739915b18b481e34ccf611f
nodays0502 <nodayst@naver.com>
2022-01-02 23:03:54 +0900

    BOJ_1259

---
+++ src/BOJ/String/BOJ_1259.java
@@ -0,0 +1,28 @@
+package BOJ.String;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_1259 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String num = br.readLine();
+        while(!"0".equals(num)){
+            int size = num.length();
+            boolean flag = true;
+            for(int i = 0 ; i < size / 2 ; i++){
+                if(num.charAt(0 + i) != num.charAt(size - 1 - i)){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                System.out.println("yes");
+            }else{
+                System.out.println("no");
+            }
+            num = br.readLine();
+        }
+    }
+}
