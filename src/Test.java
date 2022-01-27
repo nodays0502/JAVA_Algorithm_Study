7ea55ead4b6a2639a212b68df67f0b3524395031
nodays0502 <nodayst@naver.com>
2022-01-27 14:18:21 +0900

    BOJ_16927

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,11 @@
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.StringTokenizer;
 
 public class Test {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        String replace = input.replace(" ", "");
-        List<Integer> collect = Arrays.stream(replace.split(",")).map(Integer::parseInt)
-            .collect(Collectors.toList());
-        System.out.println(collect);
-    }
+    public static void main(String[] args) throws IOException {
 }
