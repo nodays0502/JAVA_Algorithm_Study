4c13df5fb77f174852e967b762ec8b58cb4dc2a6
nodays0502 <nodayst@naver.com>
2022-02-27 23:08:56 +0900

    BOJ_4673

---
+++ src/BOJ/Bruteforce/BOJ_4673.java
@@ -0,0 +1,28 @@
+package BOJ.Bruteforce;
+
+public class BOJ_4673 {
+    static final int SIZE = 10_000;
+    private static void checkSelfNumber(int number, boolean[] selfNumber){
+        int sum = number;
+        while(number > 0){
+            sum += (number % 10);
+            number /= 10;
+        }
+        if(sum <= SIZE){
+            selfNumber[sum] = true;
+        }
+    }
+    public static void main(String[] args) {
+        boolean[] selfNumber = new boolean[SIZE+1];
+        for(int i = 1; i <= SIZE; i++){
+            checkSelfNumber(i,selfNumber);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= SIZE; i++){
+            if(!selfNumber[i]){
+                sb.append(i+"\n");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
