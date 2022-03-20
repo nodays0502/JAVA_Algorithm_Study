f388ebff4690ce7ac0d76356445725125b8cb3a9
nodays0502 <nodayst@naver.com>
2022-03-20 17:37:30 +0900

    BOJ_13460

--- src/BOJ/DP/BOJ_14728.java
+++ src/BOJ/DP/BOJ_14728.java
@@ -1,2 +1,45 @@
-package BOJ.DP;public class BOJ_14728 {
+package BOJ.DP;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.function.Function;
+
+public class BOJ_14728 {
+    private static int dfs(int time, int depth, int n, int[]needTime, int[]score, int[][]dp){
+        if(depth == n){
+            return 0;
+        }
+        if(dp[time][depth] != -1){
+            return dp[time][depth];
+        }
+        int result = 0;
+        if(time >= needTime[depth]){
+            result = Math.max(result, dfs(time - needTime[depth],depth + 1,n,needTime,score,dp) + score[depth]);
+        }
+        result = Math.max(result, dfs(time,depth + 1,n,needTime,score,dp));
+        dp[time][depth] = result;
+        return result;
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        Function<String,Integer> stoi = Integer::parseInt;
+        int n = stoi.apply(st.nextToken());
+        int m = stoi.apply(st.nextToken());
+        int[] needTime = new int[n];
+        int[] score = new int[n];
+        for(int i = 0 ; i < n;  i++){
+            st = new StringTokenizer(br.readLine()," ");
+            needTime[i] = stoi.apply(st.nextToken());
+            score[i] = stoi.apply(st.nextToken());
+        }
+        int[][] dp = new int[m+1][n];
+        for(int i = 0 ; i <= m ; i++){
+            Arrays.fill(dp[i],-1);
+        }
+        System.out.println(dfs(m,0,n,needTime,score,dp));
+    }
 }
