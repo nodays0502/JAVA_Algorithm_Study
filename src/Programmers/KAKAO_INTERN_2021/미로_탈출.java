41774d25368349160494cdc14151ba9ee6cdda08
nodays0502 <nodayst@naver.com>
2022-04-21 19:13:39 +0900

    프로그래머스 미로_탈출

--- src/Programmers/KAKAO_INTERN_2021/미로_탈출.java
+++ src/Programmers/KAKAO_INTERN_2021/미로_탈출.java
@@ -1,27 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
+package Programmers.KAKAO_INTERN_2021;
+
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
-import java.util.StringTokenizer;
-import org.w3c.dom.Node;
 
-public class Test {
-
-    public static void main(String[] args) {
-        int[][] roads = new int[][] {{1,2,3},{3,2,3}};
-        int[] traps = new int[] {2};
-        System.out.println(solution(3,1,3,roads,traps));
-    }
-    static class Node{
+public class 미로_탈출 {
+    private static class Node{
         int now;
         int trap;
         int weight;
@@ -34,27 +18,34 @@ public class Test {
         public String toString(){
             return now+","+trap+","+weight;
         }
+        public String toCheckVisited(){
+            return now+","+trap;
+        }
     }
     public static int solution(int n, int start, int end, int[][] roads, int[] traps) {
         int answer = 0;
         int[][] map = new int[n+1][n+1];
-        Map<Integer,Integer>isTrap = new HashMap<>();
+        Map<Integer,Integer> isTrap = new HashMap<>();
         init(map,isTrap,traps,roads);
         answer = dij(map,isTrap,traps,start,end,n);
         return answer;
     }
+    private static final int INF = 987654321;
     private static int dij(int[][] map,Map<Integer,Integer>isTrap,int[] traps,int start,int end,int n){
-        Set<String> visited = new HashSet<>();
+        Map<String,Integer> visited = new HashMap<>();
         // boolean[][] visited = new boolean[n+1][1>>n];
         PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2)->{
             return o1.weight - o2.weight;
         });
-        pq.offer(new Node(start,0,0));
+        Node startNode = new Node(start,0,0);
+        pq.offer(startNode);
+        visited.put(startNode.toCheckVisited(),0);
         while(!pq.isEmpty()){
             Node node = pq.poll();
             System.out.println(node);
             if(node.now == end){
                 return node.weight;
+
             }
             int now = node.now;
             int weight = node.weight;
@@ -67,25 +58,20 @@ public class Test {
                 int temp = checkIsTrap(isTrap,i);
                 int tempWeight = weight;
                 if(temp != -1){
-                    tempTrap = tempTrap ^ 1 >> temp;
+                    tempTrap = tempTrap ^ (1 << temp);
                 }
-                boolean flag = false;
-                System.out.println(i+" "+map[now][i]);
-                System.out.println(Arrays.toString(nowTrap));
-                if(map[now][i] > 0 && (!nowTrap[now] || (nowTrap[now] && nowTrap[i]))){
+                if(map[now][i] > 0 && ((!nowTrap[now] && !nowTrap[i])
+                    || (nowTrap[now] && nowTrap[i]))){
                     tempWeight += map[now][i];
-                    System.out.println("1st:"+tempWeight);
-                }
-                if(nowTrap[now] && !nowTrap[i] && map[i][now] > 0 ){
+                }else if( (nowTrap[now] || nowTrap[i]) && map[i][now] > 0 ){
                     tempWeight += map[i][now];
-                    System.out.println("2nd"+tempWeight);
                 }
                 if(tempWeight == weight){
                     continue;
                 }
                 Node next = new Node(i,tempTrap,tempWeight);
-                if(!visited.contains(next.toString())){
-                    visited.add(next.toString());
+                if(visited.getOrDefault(next.toCheckVisited(),INF) > tempWeight){
+                    visited.put(next.toCheckVisited(),tempWeight);
                     pq.offer(next);
                 }
             }
@@ -97,7 +83,11 @@ public class Test {
             int a = road[0];
             int b = road[1];
             int weight = road[2];
-            map[a][b] = weight;
+            if(map[a][b] == 0){
+                map[a][b] = weight;
+            }else{
+                map[a][b] = Math.min(map[a][b],weight);
+            }
         }
         int index = 0;
         for(int trap : traps){
@@ -113,7 +103,7 @@ public class Test {
             return booleanIsTrap;
         }
         for(int i = 0 ; i < 10 ; i++){
-            if( (num & 1>>i) > 0){
+            if( (num & 1<<i) > 0){
                 int temp = traps[i];
                 booleanIsTrap[temp] = true;
             }
---
+++ src/Programmers/test_2.java
@@ -0,0 +1,14 @@
+package Programmers;
+
+public class test_2 {
+
+    public static void main(String[] args) {
+        System.out.println(1<<0);
+        System.out.println(1<<1);
+        System.out.println(1<<2);
+        System.out.println(1<<3);
+        System.out.println(1<<4);
+        System.out.println(1<<11);
+
+    }
+}
