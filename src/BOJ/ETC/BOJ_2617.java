1742eb9fb72b5314b7e1df806bc52adf70cfd33d
nodays0502 <nodayst@naver.com>
2022-04-30 20:29:29 +0900

    BOJ_2617

--- src/BOJ/ETC/BOJ_2617.java
+++ src/BOJ/ETC/BOJ_2617.java
@@ -1,17 +1,32 @@
 package BOJ.ETC;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
-import java.util.function.Function;
+    import java.io.BufferedReader;
+    import java.io.InputStreamReader;
+    import java.util.ArrayList;
+    import java.util.Arrays;
+    import java.util.LinkedList;
+    import java.util.List;
+    import java.util.Queue;
+    import java.util.StringTokenizer;
+    import java.util.function.Function;
 
 public class BOJ_2617 {
-    private static void bfs(List<Integer>[] list1, int[] cnt, int n){
-
+    private static int bfs(List<Integer>[] list, int n, int start){
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[n+1];
+        q.offer(start);
+        int result = 0;
+        while(!q.isEmpty()){
+            int now = q.poll();
+            for(int next : list[now]){
+                if(!visited[next]){
+                    visited[next] = true;
+                    q.offer(next);
+                    result++;
+                }
+            }
+        }
+        return result;
     }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,14 +36,28 @@ public class BOJ_2617 {
         int m = stoi.apply(st.nextToken());
         List<Integer>[] lighter = new List[n+1];
         List<Integer>[] heavier = new List[n+1];
+        for(int i = 0 ; i <= n ; i++){
+            lighter[i] = new ArrayList<>();
+            heavier[i] = new ArrayList<>();
+        }
         for(int i = 0 ; i < m ; i++){
+            st = new StringTokenizer(br.readLine()," " );
             int a = stoi.apply(st.nextToken());
             int b = stoi.apply(st.nextToken());
-            heavier[a].add(b);
-            lighter[b].add(a);
+            heavier[b].add(a); // a > b
+            lighter[a].add(b);
         }
         int[] lightCnt = new int[n+1];
         int[] heavyCnt = new int[n+1];
-
+        int result = 0;
+        int limit = n/2 + 1;
+        for(int i = 1 ; i <= n ; i ++){
+            heavyCnt[i] = bfs(heavier,n,i);
+            lightCnt[i] = bfs(lighter,n,i);
+            if(heavyCnt[i] >= limit || lightCnt[i] >= limit){
+                result++;
+            }
+        }
+        System.out.println(result);
     }
 }
