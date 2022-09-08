40794f121ba14717ca199489e2cde861c2a8aea5
nodays0502 <nodayst@naver.com>
2022-09-08 21:06:52 +0900

    프로그래머스 자릿수 더하기

---
+++ src/Programmers/ETC/자릿수_더하기.java
@@ -0,0 +1,12 @@
+package Programmers.ETC;
+
+public class 자릿수_더하기 {
+    public int solution(int n) {
+        int answer = 0;
+        while(n > 0){
+            answer += (n % 10);
+            n = n / 10;
+        }
+        return answer;
+    }
+}
