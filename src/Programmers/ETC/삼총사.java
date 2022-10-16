1fd3ccc943f2ddd1af66ad4ecab85d43591711ad
nodays0502 <nodayst@naver.com>
2022-10-16 22:47:29 +0900

    프로그래머스 삼총사

---
+++ src/Programmers/ETC/삼총사.java
@@ -0,0 +1,18 @@
+package Programmers.ETC;
+
+public class 삼총사 {
+    public int solution(int[] number) {
+        int answer = 0;
+        int size = number.length;
+        for(int i = 0 ; i < size ; i++){
+            for(int j = i+1 ; j < size ; j++){
+                for(int k = j+1 ; k < size ; k++){
+                    if(number[i] + number[j] + number[k] == 0){
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
