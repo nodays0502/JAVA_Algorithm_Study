c417421bb84876096fe2cea6a73a4c60af825df3
nodays0502 <nodayst@naver.com>
2022-11-29 19:25:44 +0900

    프로그래머스 한번만

---
+++ src/Programmers/ETC/한_번만_등장한_문자.java
@@ -0,0 +1,20 @@
+package Programmers.ETC;
+
+public class 한_번만_등장한_문자 {
+    public String solution(String s) {
+        String answer = "";
+        Map<Character,Integer> cnt = new HashMap<>();
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            cnt.merge(ch,1,(v1,v2)-> v1+1);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char ch = 'a' ; ch <= 'z' ; ch++){
+            if(cnt.getOrDefault(ch,0) == 1){
+                sb.append(ch);
+            }
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
