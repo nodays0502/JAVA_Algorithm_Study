17daf01e1bda3bc847a106ce9ca23a1d67fd7f5e
nodays0502 <nodayst@naver.com>
2023-01-24 19:12:15 +0900

    프로그래머스 직각삼각형 출력하기

---
+++ src/Programmers/ETC/직각삼각형_출력하기.java
@@ -0,0 +1,16 @@
+package Programmers.ETC;
+
+import java.util.Scanner;
+
+public class 직각삼각형_출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j <= i ; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
