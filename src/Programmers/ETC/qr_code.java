b983200e9d9e4ee4322173efd266f04369fb20d1
nodays0502 <nodayst@naver.com>
2023-04-27 21:59:55 +0900

    프로그래머스 qr code

---
+++ src/Programmers/ETC/qr_code.java
@@ -0,0 +1,15 @@
+package Programmers.ETC;
+
+public class qr_code {
+    public String solution(int q, int r, String code) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < code.length() ; i++){
+            if(i % q == r){
+                sb.append(code.charAt(i));
+            }
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
