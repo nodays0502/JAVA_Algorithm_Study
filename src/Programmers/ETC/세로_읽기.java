f106a1357168fd3db0228d179d7e5725a12cff30
nodays0502 <nodayst@naver.com>
2023-05-24 20:22:24 +0900

    프로그래머스 세로 읽기

---
+++ src/Programmers/ETC/세로_읽기.java
@@ -0,0 +1,13 @@
+package Programmers.ETC;
+
+public class 세로_읽기 {
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+        for(int i = 0 ; i < my_string.length() ; i++){
+            if( (i+1) % m == c || m == 1 || ((i+1) % m == 0 && c == m) ){
+                answer += my_string.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
