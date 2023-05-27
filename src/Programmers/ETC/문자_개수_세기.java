4275f30c183b84f619e60e95c2660527204ef864
nodays0502 <nodayst@naver.com>
2023-05-27 23:15:24 +0900

    프로그래머스 문자 개수 세기

---
+++ src/Programmers/ETC/문자_개수_세기.java
@@ -0,0 +1,18 @@
+package Programmers.ETC;
+
+public class 문자_개수_세기 {
+    private static final int SIZE = 26;
+    public int[] solution(String my_string) {
+        int[] answer = new int[2 * SIZE];
+        for(int i = 0 ; i < my_string.length() ; i++){
+            char ch = my_string.charAt(i);
+            if(ch >= 'a' && ch <= 'z'){
+                answer[ch - 'a' + SIZE]++;
+            }
+            if(ch >= 'A' && ch <= 'Z'){
+                answer[ch - 'A']++;
+            }
+        }
+        return answer;
+    }
+}
