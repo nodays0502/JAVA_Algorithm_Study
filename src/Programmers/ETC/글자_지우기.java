5ad4f77226de5ae5cb2aae0440375b5f4a3ebab7
nodays0502 <nodayst@naver.com>
2023-06-27 20:18:56 +0900

    프로그래머스 글자 지우기

---
+++ src/Programmers/ETC/글자_지우기.java
@@ -0,0 +1,20 @@
+package Programmers.ETC;
+
+public class 글자_지우기 {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        boolean[] isDeleted = new boolean[my_string.length()];
+        for(int index : indices){
+            isDeleted[index] = true;
+        }
+        for(int i = 0 ; i < my_string.length() ; i++){
+            if(isDeleted[i]){
+                continue;
+            }
+            sb.append(my_string.charAt(i));
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
