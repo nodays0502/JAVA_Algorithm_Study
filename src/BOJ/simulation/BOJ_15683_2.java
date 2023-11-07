65c6169169b033b5d116151e76abb4c38321ab2e
nodays0502 <nodayst@naver.com>
2023-11-07 17:40:20 +0900

    BOJ_15683

--- src/BOJ/simulation/BOJ_15683_2.java
+++ src/BOJ/simulation/BOJ_15683_2.java
@@ -1,13 +1,12 @@
+package BOJ.simulation;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Test {
+public class BOJ_15683_2 {
 
     private static final int[][] CCTV_DIR = {
         {}, // DUMMY
@@ -84,4 +83,3 @@ public class Test {
         return result;
     }
 }
-
