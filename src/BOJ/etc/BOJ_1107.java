4e97569e5280a01a8565681b6011a7407842a95b
nodays0502 <nodayst@naver.com>
2023-11-09 19:05:40 +0900

    BOJ_1107

--- src/BOJ/etc/BOJ_1107.java
+++ src/BOJ/etc/BOJ_1107.java
@@ -1,13 +1,12 @@
+package BOJ.etc;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-public class Test {
-
+public class BOJ_1107 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -55,4 +54,3 @@ public class Test {
     }
 
 }
-
