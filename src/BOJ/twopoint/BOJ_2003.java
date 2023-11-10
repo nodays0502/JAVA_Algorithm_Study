351d489fb652b37d5d630e80c3b78348160ff048
nodays0502 <nodayst@naver.com>
2023-11-10 16:19:23 +0900

    BOJ_2003

--- src/BOJ/twopoint/BOJ_2003.java
+++ src/BOJ/twopoint/BOJ_2003.java
@@ -1,13 +1,10 @@
+package BOJ.twopoint;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
-public class Test {
-
+public class BOJ_2003 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -42,6 +39,4 @@ public class Test {
         }
         return cnt;
     }
-
 }
-
